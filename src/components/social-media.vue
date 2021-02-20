db8d0431b8a04251d21a2edda5d953082db00b0c
Ilaria Olimpia <ilaria.posca@gmail.com>
2021-02-20 11:17:14 +0100

    optimize seo

--- src/components/social-media.vue
+++ src/components/social-media.vue
@@ -2,27 +2,32 @@
     <nav class="social-media-nav">
         <ul>
           <li>
-              <a href="" target="_blank" alt="" class="social-link">
+              <a href="https://www.linkedin.com/in/ilariaolimpia-posca/" target="_blank" alt="LinkedIn" class="social-link">
                   <span>Linkedin</span>                  
               </a>
           </li>
           <li>
-              <a href="" target="_blank" alt="" class="social-link">
+              <a href="https://www.facebook.com/ilarjaolympia.poska/" target="_blank" alt="Facebook" class="social-link">
                   <span>Facebook</span>                  
               </a>
           </li>
           <li>
-              <a href="" target="_blank" alt="" class="social-link">
+              <a href="https://www.instagram.com/eroenegativo/" target="_blank" alt="Instagram" class="social-link">
                   <span>Instagram</span>                  
               </a>
           </li>
           <li>
-              <a href="" target="_blank" alt="" class="social-link">
+              <a href="https://www.flickr.com/photos/93959529@N05/" target="_blank" alt="Flickr" class="social-link">
+                  <span>Flickr</span>                  
+              </a>
+          </li>
+          <li>
+              <a href="https://codepen.io/eroenegativo" target="_blank" alt="Codepen" class="social-link">
                   <span>Codepen</span>                  
               </a>
           </li>
             <li>
-              <a href="" target="_blank" alt="" class="social-link">
+              <a href="https://github.com/eroenegativo" target="_blank" alt="Github" class="social-link">
                   <span>Github</span>                  
               </a>
           </li>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,51 +1,67 @@
 <template>
-  <section v-onscroll id="about">
-     <!--<h2 class="section-title">About me</h2>-->
+  <section v-onscroll id="about" itemscope itemtype="https://schema.org/Person">
+    <!--<h2 class="section-title">About me</h2>-->
     <div class="column align-center">
       <div class="box">
-          <div class="box-wrapper">
-              <div class="box-description">
-                <p> Hello world, <br/>
-                    i'm IO a technology enthusiast <b>Front-end Developer</b> working as a consultant in my hometown, Milan.<br/>
-                    My skills are a perfect compromise between good design and technology.
-                </p>
-              </div>
-              <div class="box-button">
-                  <ul>
-                    <li>
-                      <a href="../assets/Ilaria_Olimpia_Posca_Resume_202001.pdf" target="_blank" alt="Currriculum Viate" download> 
-                        <span>Download my CV</span> 
-                        <font-awesome-icon :icon="['fa', 'download']" />
-                      </a>
-                    </li>
-                  </ul>
-              </div>
+        <div class="box-wrapper">
+          <div class="box-description">
+            <p>
+              Hello world, <br />
+              i'm <span itemprop="name">Ilaria Olimpia Posca</span> a technology enthusiast <b itemprop="jobTitle">Front-end Developer</b> working mainly
+              as a <span itemprop="worksFor">consultant developer</span> in my hometown, <span itemprop="birthPlace">Milan</span>.<br />
+            </p>
           </div>
-      </div>
-        <div class="box">
-          <div class="box-wrapper">
-              <div class="box-description">
-                <p> My background mostly focused on art and design<br/>
-                </p>
-              </div>
-               <div class="box-button">
-                  <ul>
-                    <li>
-                    <a href="https://www.linkedin.com/in/ilariaolimpia-posca/" target="_blank" alt="linkedin" >
-                        <span>Linkedin</span>
-                      <font-awesome-icon :icon="['fa', 'external-link-alt']" />
-                    </a>
-                  </li>
-                  <li>
-                      <a href="https://www.instagram.com/eroenegativo/" target="_blank" alt="Instagram">
-                        <span>Instagram </span>
-                        <font-awesome-icon :icon="['fa', 'external-link-alt']" />
-                      </a>
-                  </li>
-                  </ul>
-              </div>
+          <div class="box-button">
+            <ul>
+              <li>
+                <a
+                  href="../assets/Ilaria_Olimpia_Posca_Resume_202001.pdf"
+                  target="_blank"
+                  alt="Currriculum Viate"
+                  download
+                >
+                  <span>Download my CV</span>
+                  <font-awesome-icon :icon="['fa', 'download']" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
+      </div>
+      <div class="box">
+        <div class="box-wrapper">
+          <div class="box-description">
+            <p>
+              My background is focused on design and art,<br /> i have a Bachelor's
+              degree at <b itemprop="alumniOf">IED (European Disgn Institute)</b>.
+            </p>
+          </div>
+          <div class="box-button">
+            <ul>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/ilariaolimpia-posca/"
+                  target="_blank"
+                  alt="Linkedin"
+                >
+                  <span>Linkedin</span>
+                  <font-awesome-icon :icon="['fa', 'external-link-alt']" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/eroenegativo/"
+                  target="_blank"
+                  alt="Instagram"
+                >
+                  <span>Instagram </span>
+                  <font-awesome-icon :icon="['fa', 'external-link-alt']" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
