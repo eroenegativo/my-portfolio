784e959ccfc7c429fff0b8c59f05ca36f76529b4
Ilaria Olimpia <ilaria.posca@gmail.com>
2021-06-24 18:07:08 +0200

    bugfix on link

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@
     <div class="main-nav">
             <router-link to="/about" class="nav-link">
                 <span>Who am i?</span>
-                <svg id="stroke" xmlns="http://www.w3.org/2000/svg" width="0" height="0">
-  <defs>
-    <path id="line" d="M2 2c49.7 2.6 100 3.1 150 1.7-46.5 2-93 4.4-139.2 7.3 45.2-1.5 90.6-1.8 135.8-.6" fill="none" stroke-linecap="round" stroke-linejoin="round" vector-effect="non-scaling-stroke"/>
-  </defs>
-</svg>
             </router-link>
     </div>
     <router-view/>
